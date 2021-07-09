151a1fbf13ffac2f85bd35253ede6ac43a508a01
채희찬 <eqholic1125@gmail.com>
2021-07-09 23:48:31 +0900

    events

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const title = document.querySelectorAll(".hello h1");
+const title = document.querySelector(".hello:first-child h1");
 
-console.log(title);
+function handleTitleClick(){
+    title.style.color="blue";
+}
 
+title.addEventListener("click", handleTitleClick);
