b519d893c4ffb59c768f97454146f2213680f5ea
Chae hee chan <eqholic1125@gmail.com>
2021-08-01 18:36:19 +0900

    css in js

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick(){
-    h1.style.color="blue";
+    const currnetColor = h1.style.color;
+    let newColor;
+    if(currnetColor !== "blue"){
+        newColor = "blue";
+    }
+    else{
+        newColor = "tomato";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){
-    h1.innerText = "mouse is here";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOD");
-}
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
