cdeadd0005399a47b207cfe0c44eff96090575a7
채희찬 <eqholic1125@gmail.com>
2021-08-03 23:32:00 +0900

    parameter

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const loginInput = document.querySelector("#login-form input");
 function onLoginSubmit(event){
     event.preventDefault();
     console.log(loginInput.value)
+    // 공부합시다
 } 
 
 loginForm.addEventListener("submit", onLoginSubmit);
