bc6f23fdc369c8e6315ae01b92f6c06d7be3f944
Chae hee chan <eqholic1125@gmail.com>
2021-10-07 20:30:12 +0900

    load username

--- script.js
+++ script.js
@@ -3,13 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem("userName", loginInput.value);
-    greeting.innerText = `Hello ${localStorage.getItem("userName")}!`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreeting(savedUserName);
 } 
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username){
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+console.log(savedUserName);
+
+// userName이 없을 경우
+if (savedUserName == null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{  // userName이 있을 경우
+    paintGreeting(savedUserName);
+}
